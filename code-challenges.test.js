1335f1ef2edbc1128d69d25ec1995bfaf3ede1b9
spiralcode28 <learnacademy@LEARNs-MacBook-Air.local>
2022-01-03 01:22:57 -0800

    Finished Assessment 3.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,7 +10,6 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
@@ -21,36 +20,146 @@
 // Example input: 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+// a describe method that lists a function with expect statements
+
+describe("fibonacciLength", () => {
+  it("takes in a number >2 and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+    // expected input: 6, expected output: our example values above
+    // expected input: 10, expected output: our example values above
+
+    expect(fibonacciLength(6)).toEqual([1, 1, 2, 3, 5, 8]);
+    expect(fibonacciLength(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+  });
+});
+
+// Run file in jest
+// RED: failed test - fibonacciLength is not defined
 
 // b) Create the function that makes the test pass.
 
+// create a function that takes in a num as a parameter
+
+var input1 = 6;
+var input2 = 10;
+
+// create a function that takes in a number and iterates through it
+
+// create a variable that will hold our finished array
+
+const fibonacciLength = (num) => {
+  let fibArray = [1, 1];
+
+  // iterate through the new array and have its index value equal the index's previous iteration and add the two iterations before.
+
+  for (let i = 2; i < num; i++) {
+    fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+  }
+  return fibArray;
+};
+
+// console.log(fibonacciLength(input1));
+
+// Run file in jest
+// GREEN: passed test
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"];
 // Expected output: [-9, 7, 9, 199]
 
-var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"];
 // Expected output: [-823, 7, 23]
 
+// a describe method that lists a function with expect statements
+
+describe("oddNums", () => {
+  it("takes in an array and returns a new array of only odd numbers sorted from least to greatest", () => {
+    // input will be our fullArr variables. The output will be the expected output arrays.
+
+    expect(oddNums(fullArr1)).toEqual([-9, 7, 9, 199]);
+    expect(oddNums(fullArr2)).toEqual([-823, 7, 23]);
+  });
+});
+
+// Run file in jest
+//RED: failed test - oddNums is not defined
 
 // b) Create the function that makes the test pass.
 
+// create a function that takes in an array then filters out other data except numbers, store in variable
+
+// filter out even numbers and keep only odd, store in new variable
+
+// take newest array and .sort from lowest to highest
+
+// refactor code (applied .filter twice the first variable and deleted the second)
+
+const oddNums = (array) => {
+  let newArr = array
+    .filter((value) => typeof value === "number")
+    .filter((value) => {
+      return value % 2 !== 0 ? value : null;
+    });
+  return newArr.sort((firstItem, secondItem) => firstItem - secondItem);
+};
+
+// console.log(oddNums(fullArr1));
+// console.log(oddNums(fullArr2));
+
+// Run file in jest
+// GREEN: passed test
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var numbersToAdd1 = [2, 4, 45, 9]
+var numbersToAdd1 = [2, 4, 45, 9];
 // Excpected output: [2, 6, 51, 60]
 
-var numbersToAdd2 = [0, 7, -8, 12]
+var numbersToAdd2 = [0, 7, -8, 12];
 // Expected output: [0, 7, -1, 11]
 
-var numbersToAdd3 = []
+var numbersToAdd3 = [];
 // Expected output: []
 
+describe("addArray", () => {
+  it("function that takes in an array and returns an array of the accumulating sum, empty array should return an empty array", () => {
+    // input will be our example variables (numbersToAdd). The output will be our expected output values.
+
+    expect(addArray(numbersToAdd1)).toEqual([2, 6, 51, 60]);
+    expect(addArray(numbersToAdd2)).toEqual([0, 7, -1, 11]);
+    expect(addArray(numbersToAdd3)).toEqual([]);
+  });
+});
+
+// Run file in jest
+// RED: failed test - addArray is not defined
 
 // b) Create the function that makes the test pass.
+
+const addArray = (array) => {
+  // declare a variable containing an empty array.
+
+  let newArr = [];
+  // use reduce method to iterate through the array
+
+  // declare the use of previous, current, and index values
+
+  // have newArr index value equal the arrays previous value plus the current iteration value
+
+  // function will return empty if no values are present in the array
+
+  array.reduce(
+    (previousVal, currentVal, i) => (newArr[i] = previousVal + currentVal),
+    0
+  );
+  return newArr;
+};
+// console.log(plusArray(numbersToAdd1));
+// console.log(plusArray(numbersToAdd2));
+// console.log(plusArray(numbersToAdd3));
+
+// Run file in jest
+// GREEN: passed test
--- interview-questions.md
+++ interview-questions.md
@@ -6,62 +6,68 @@ First, without external resources. Challenge yourself to answer from memory as i
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-
 1. What is `this` in JavaScript?
 
-  Your answer:
+Your answer:
+'This', used in javascript, points to a declared variable within a class. We can then use that declared variable within a method which is also used in the class.
 
-  Researched answer:
-
-
+Researched answer:
+It allows us to use the declared variable within a method also used in the class. If 'this' is used inside a method it points to the owner object. It can also be used on it's own when refering to the global object.
 
 2. What is React? Why would you use it?
 
-  Your answer:
+Your answer:
+React is a software created initially for Facebook, that contains a pre-built library for developers to use. The reason it was built was so web-pages could update and data could also be manipulated without the page refreshing. Refreshing a page is time and money.
 
-  Researched answer:
-
-
+Researched answer:
+It is also component-based. The reason so many developers use it is because of how simple (and fun) it is to create interactive UIs.
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+Your answer:
+Render() is a required lifecycle method in a React class component.
 
-  Researched answer:
-
-
+Researched answer:
+Any of the other methods are optional in a React class component, but you must include render().
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+Your answer:
+JSX is a language that allows you to use html within a Javascript file. html can be written only in between a set of JSX brackets.
 
-  Researched answer:
-
-
+Researched answer:
+It stands for JavaScript Syntax Extension, essentially writing in both HTML and Javascript. The main difference between HTML and JSX is that you must return only one parent element if you wnat it to compile.
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+Your answer:
+Yarn is a package manager for your code. that allows for developers to download packages directly into a project for use. There are a variety of different uses such as testing in jest or with a react project.
 
-  Researched answer:
-
-
+Researched answer:
+Yarn is usually ran in terminal and brings over set packaged files for developers. Besides being a package mananger it is also a project manager.
+Developers can download plug-in features for better work flow and even share their code with others seemlessly.
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+Your answer:
+I would have to google it. I don't believe I have dabbled with them.
 
-  Researched answer:
-
+Researched answer:
+An anonymous function within Javascript is a nameless function, usually stored in a variable and is called upon ONLY by the variable name.
 
 ## Looking Ahead: Terms for Next Week
 
 1. Conditional rendering:
+   Conditional rendering is the ability to render different user interface markup if a condition is true or false.
 
 2. Props:
+   Props are properties that share data between components.
 
 3. JavaScript Events:
+   When Javascript is activated within an html page.
 
 4. Object-oriented programming:
+   OOP for short, is a programming model that organizes software design around data or objects and not functions and logic.
 
 5. Ruby:
+   Ruby is an interpreted, high-level, general-purpose programming language which supports multiple programming paradigms.
